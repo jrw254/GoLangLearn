363523239bf328adea4ad8b82b8f0e10fd151d5d
jrw254 <47221007+jrw254@users.noreply.github.com>
2021-04-10 23:59:59 -0500

    Create timerKeypress.go
    Basic timer that loops for 4 hours. Every 7.7 minutes it sends a Keypress.

---
+++ timerKeypress.go
@@ -0,0 +1,64 @@
+package main 
+import 
+(   
+	  "fmt"
+    "log"    
+    "syscall"     
+    "time"     
+    "unsafe" 
+) 
+
+var ( 
+    user32 = syscall.NewLazyDLL("user32.dll")     
+    sendInputProc = user32.NewProc("SendInput") ) 
+    
+func keyPress() {     
+    type keyboardInput struct {         
+        wVk uint16         
+        wScan uint16         
+        dwFlags uint32         
+        time uint32         
+        dwExtraInfo uint64     
+    }     
+
+    type input struct {         
+        inputType uint32         
+        ki keyboardInput         
+        padding uint64     
+    }     
+    
+    var i input     
+    i.inputType = 1 // INPUTKEYBOARD     
+    i.ki.wVk = 0x87 // virtual key code for F24
+    lop, _, err := sendInputProc.Call(         
+        uintptr(1),
+        uintptr(unsafe.Pointer(&i)),
+        uintptr(unsafe.Sizeof(i)),     
+        )
+		log.Printf("%v %v", lop, err)
+    } 
+	
+    func main() {
+		allDone := make(chan bool)  
+		tick := time.NewTicker(462 * time.Second) // --7.7 minutes or 462 Second, each loop length
+		tn := time.Now()
+			fmt.Printf("%s", tn)
+			fmt.Println("\n.....Initializing.....")
+			go func() {
+				for {
+					select {
+					case <-allDone:
+							return
+					case  <-tick.C: 
+						fmt.Println("\n In Session: Your Timer:")
+						keyPress()
+						fmt.Println("")
+					case <-time.After(1 * time.Minute): // Running Log Here. Change Time to whatever
+						log.Printf("Running...")
+					}
+				}	
+			}()
+			time.Sleep(4 * time.Hour)  // Length of time till it stops/ends
+			allDone <- true
+			fmt.Println("Done for the day!!!!!!!!!")
+    }     
